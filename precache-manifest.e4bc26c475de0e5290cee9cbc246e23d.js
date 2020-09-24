1fbc5b60ed608bbf6c8fc12f3a2184726469852c
Harvey Reynier <harveyreynier@gmail.com>
2020-09-24 12:52:03 +0200

    Updates

---
+++ precache-manifest.e4bc26c475de0e5290cee9cbc246e23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccba206895b38c7e21bc02a475d1cc7",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "dfa11fc8d3db9f53cddd",
+    "url": "/simple-todo-app/static/css/main.1eacbda4.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa11fc8d3db9f53cddd",
+    "url": "/simple-todo-app/static/js/main.ae80eb02.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
